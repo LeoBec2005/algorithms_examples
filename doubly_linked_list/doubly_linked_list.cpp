ed66e6e309af38d013d33a2771c8521001184dd2
Leonardo Becerra <leonardobecerra38@gmail.com>
2025-09-07 15:23:48 -0500

    feat: add destructor, split print into forward/backward, and improve naming

--- doubly_linked_list/doubly_linked_list.cpp
+++ doubly_linked_list/doubly_linked_list.cpp
@@ -3,20 +3,33 @@
 struct Node {
     int data;
     Node* next = nullptr;
-    Node* forward = nullptr;
+    Node* prev = nullptr;
 };
 
 struct List {
     Node* head = nullptr;
     Node* tail = nullptr;
+    
+    ~List(){
+        if (head == nullptr) return;
+        Node* temp = head;
+        while (temp != nullptr) {
+            Node* temp2 = temp;
+            temp = temp->next;
+            delete temp2;
+        }
+        std::cout << "list destroyed!"<<std::endl;
+    } 
 
-    void print() {
+    void print_forward() const {
         for (Node* temp = head; temp != nullptr; temp = temp->next) {
             std::cout << temp->data << " , ";
         }
         std::cout << std::endl;
+    }
 
-        for (Node* temp = tail; temp != nullptr; temp = temp->forward) {
+    void print_backward() const {
+        for (Node* temp = tail; temp != nullptr; temp = temp->prev) {
             std::cout << temp->data << " , ";
         }
         std::cout << std::endl;
@@ -32,7 +45,7 @@ struct List {
         else {
             Node* temp = head;
             head = new_node;
-            temp->forward = new_node;
+            temp->prev = new_node;
             new_node->next = temp;
         }
     }
@@ -48,21 +61,23 @@ struct List {
             Node* temp = tail;
             tail = new_node;
             temp->next = new_node;
-            new_node->forward = temp;
+            new_node->prev = temp;
         }
     }
 
     void pop_back() {
+        if (head == nullptr) return;
         Node* temp = tail;
-        tail = tail->forward;
+        tail = tail->prev;
         tail->next = nullptr;
         delete temp;
     }
 
     void pop_front() {
+        if (head == nullptr) return;
         Node* temp = head;
         head = head->next;
-        head->forward = nullptr;
+        head->prev = nullptr;
         delete temp;
     }
 
@@ -78,12 +93,14 @@ int main() {
     list_1.push_back(7);
     list_1.push_back(8);
     list_1.push_back(9);
-    list_1.print();
+    list_1.print_backward();
+    list_1.print_forward();
 
     list_1.pop_back();
     list_1.pop_back();
     list_1.pop_front();
     list_1.pop_front();
-    list_1.print();
+    list_1.print_backward();
+    list_1.print_forward();
 
 }
