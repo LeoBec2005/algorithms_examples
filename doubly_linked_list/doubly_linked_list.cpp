ae3c6eb97a7e1c886c0d4e66e4a491191160adb5
Leonardo Becerra <leonardobecerra38@gmail.com>
2025-09-12 16:18:57 -0500

    feat: add clear, find, list_length and insert methods
    - Added clear, find, list_length and insert
    - Fixed single-node case in pop_front and pop_back
    - Refactored destructor to reuse clear()

--- doubly_linked_list/doubly_linked_list.cpp
+++ doubly_linked_list/doubly_linked_list.cpp
@@ -11,14 +11,7 @@ struct List {
     Node* tail = nullptr;
     
     ~List(){
-        if (head == nullptr) return;
-        Node* temp = head;
-        while (temp != nullptr) {
-            Node* temp2 = temp;
-            temp = temp->next;
-            delete temp2;
-        }
-        std::cout << "list destroyed!"<<std::endl;
+        clear();
     } 
 
     void print_forward() const {
@@ -43,10 +36,9 @@ struct List {
             tail = new_node;
         }
         else {
-            Node* temp = head;
+            head->prev = new_node;
+            new_node->next = head;
             head = new_node;
-            temp->prev = new_node;
-            new_node->next = temp;
         }
     }
 
@@ -58,15 +50,19 @@ struct List {
             tail = new_node;
         }
         else {
-            Node* temp = tail;
+            tail->next = new_node;
+            new_node->prev = tail;
             tail = new_node;
-            temp->next = new_node;
-            new_node->prev = temp;
         }
     }
 
     void pop_back() {
         if (head == nullptr) return;
+        if (head == tail) {
+            delete head;
+            head = tail = nullptr;
+            return;
+        }
         Node* temp = tail;
         tail = tail->prev;
         tail->next = nullptr;
@@ -75,10 +71,69 @@ struct List {
 
     void pop_front() {
         if (head == nullptr) return;
+        if (head == tail) {
+            delete head;
+            head = tail = nullptr;
+            return;
+        }
         Node* temp = head;
         head = head->next;
         head->prev = nullptr;
         delete temp;
+    }
+
+    bool find(int x) {
+        for (Node* temp = head; temp != nullptr; temp = temp->next) {
+            if (temp->data == x) return 1;
+        }
+        return 0;
+    }
+
+    void clear() {
+        if (head == nullptr) return;
+        Node* temp = head;
+        while (temp != nullptr) {
+            Node* temp2 = temp;
+            temp = temp->next;
+            delete temp2;
+        }
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    int list_length() {
+        int length = 0;
+        for (Node* temp = head; temp != nullptr; temp = temp->next) {
+            length++;
+        }
+        return length;
+    }
+
+    void insert(int pos, int x) {
+        if (pos > list_length() or pos < 0) {
+            std::cout << "the position is not valid!" << std::endl;
+            return;
+        }
+        Node* new_node = new Node;
+        new_node->data = x;
+        Node* temp = head;
+        for (int i = 0; i < pos; i++) {
+            temp = temp->next;
+        }
+        
+        if (temp == head) {
+            push_front(x);
+        }
+        else if (temp == tail->next) {
+            push_back(x);
+        }
+        else {
+            new_node->prev = temp->prev;
+            new_node->prev->next = new_node;
+            temp->prev = new_node;
+            new_node->next = temp;
+        }
+
     }
 
 };
@@ -96,11 +151,36 @@ int main() {
     list_1.print_backward();
     list_1.print_forward();
 
+
     list_1.pop_back();
     list_1.pop_back();
     list_1.pop_front();
     list_1.pop_front();
-    list_1.print_backward();
     list_1.print_forward();
 
+    list_1.insert(2, 10);
+    list_1.print_forward();
+
+    list_1.insert(3, 11);
+    list_1.print_forward();
+
+    list_1.insert(0, 12);
+    list_1.print_forward();
+
+    list_1.insert(7, 13);
+    list_1.print_forward();
+
+    list_1.insert(100, 14);
+    list_1.print_forward();
+
+    list_1.insert(-100, 15);
+    list_1.print_forward();
+
+    std::cout << list_1.list_length() << std::endl;
+
+    std::cout << list_1.find(4) << std::endl;
+    std::cout << list_1.find(2) << std::endl;
+    std::cout << list_1.find(6) << std::endl;
+    std::cout << list_1.find(100) << std::endl;
+    
 }
