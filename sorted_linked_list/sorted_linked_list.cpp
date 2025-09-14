7e56699e6d17ebc86e1a18a28f256226d745e02e
Leonardo Becerra <leonardobecerra38@gmail.com>
2025-09-13 22:04:52 -0500

    feat: implement SortedList with ordered insertion and print methods

--- sorted_linked_list/sorted_linked_list.cpp
+++ sorted_linked_list/sorted_linked_list.cpp
@@ -39,11 +39,52 @@ struct SortedList {
         head = nullptr;
         tail = nullptr;
     }
+
+    void insert(int x) {
+        Node* new_node = new Node;
+        new_node->data = x;
+
+        if (head == nullptr) {
+            head = new_node;
+            tail = new_node;
+            return;
+        }
+
+        Node* temp = head;
+        while(temp != nullptr and temp->data < x) {
+            temp = temp->next;
+        }
+
+        if (temp == head) {
+            head->prev = new_node;
+            new_node->next = head;
+            head = new_node;
+        }
+        else if (temp == tail->next) {
+            tail->next = new_node;
+            new_node->prev = tail;
+            tail = new_node;
+        }
+        else {
+            new_node->prev = temp->prev;
+            new_node->prev->next = new_node;
+            temp->prev = new_node;
+            new_node->next = temp;
+        }
+    }
 };
 
 
 int main()
 {
+    SortedList list_1;
+    list_1.insert(4);
+    list_1.insert(7);
+    list_1.insert(1);
+    list_1.insert(0);
+    list_1.insert(3);
+    list_1.print_forward();
+    list_1.print_backward();
 
 }
 
