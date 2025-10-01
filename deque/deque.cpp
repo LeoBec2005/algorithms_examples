63e9cabdc00e75667466a02d839b03bb4c715bd3
Leonardo Becerra <leonardobecerra38@gmail.com>
2025-09-30 21:18:44 -0500

    style: fix code formatting in Deque

--- deque/deque.cpp
+++ deque/deque.cpp
@@ -1,156 +1,156 @@
 #include <iostream>
 
 struct Deque {
-    int map_size = 6;
-    int block_size = 5;
-    int start_block = map_size / 2;
-    int finish_block = map_size / 2;
-    int start_offset = block_size /2;
-    int finish_offset = (block_size / 2)+1;
-    int** map = new int*[map_size]();
+	int map_size = 6;
+	int block_size = 5;
+	int start_block = map_size / 2;
+	int finish_block = map_size / 2;
+	int start_offset = block_size / 2;
+	int finish_offset = (block_size / 2) + 1;
+	int** map = new int* [map_size]();
 
-    Deque() {
-        map[start_block] = new int[block_size]();
-    }
+	Deque() {
+		map[start_block] = new int[block_size]();
+	}
 
-    void print() {
-        int j = start_offset+1;
-        for(int i = start_block; i <= finish_block;i++){
-            while (j < block_size) {
-                if (i >= finish_block and j >= finish_offset){
-                    std::cout<<std::endl;
-                    return;
-                }
-                std::cout << map[i][j] << " , ";
-                j++;
-            }
-            std::cout<<" | ";
-            j = 0;
-        }
-    }
+	void print() {
+		int j = start_offset + 1;
+		for (int i = start_block; i <= finish_block; i++) {
+			while (j < block_size) {
+				if (i >= finish_block and j >= finish_offset) {
+					std::cout << std::endl;
+					return;
+				}
+				std::cout << map[i][j] << " , ";
+				j++;
+			}
+			std::cout << " | ";
+			j = 0;
+		}
+	}
 
-    void resize(){
-        int new_map_size = map_size * 2;
-        int new_finish_block = (new_map_size/2) + (finish_block - (map_size/2));
-        int new_start_block = (new_map_size/2) - ((map_size/2) - start_block);
-        int** new_map = new int*[new_map_size]();
+	void resize() {
+		int new_map_size = map_size * 2;
+		int new_finish_block = (new_map_size / 2) + (finish_block - (map_size / 2));
+		int new_start_block = (new_map_size / 2) - ((map_size / 2) - start_block);
+		int** new_map = new int* [new_map_size]();
 
-        for (int i = 0; i<= finish_block-start_block; i++){
-            new_map[new_start_block+i] = map[start_block+i];
-        }
+		for (int i = 0; i <= finish_block - start_block; i++) {
+			new_map[new_start_block + i] = map[start_block + i];
+		}
 
-        //delete[] map;
+		//delete[] map;
 
-        map = new_map;
-        map_size = new_map_size;
-        start_block = new_start_block;
-        finish_block = new_finish_block;
-        std::cout<<"map resized!"<<std::endl;
-    }
+		map = new_map;
+		map_size = new_map_size;
+		start_block = new_start_block;
+		finish_block = new_finish_block;
+		std::cout << "map resized!" << std::endl;
+	}
 
-    void push_front(int x) {
-        if (start_block < 0) {
-            resize();
-        }
+	void push_front(int x) {
+		if (start_block < 0) {
+			resize();
+		}
 
-        map[start_block][start_offset] = x;
-        start_offset--;
+		map[start_block][start_offset] = x;
+		start_offset--;
 
-        if (start_offset < 0) {
-            start_block--;
-            start_offset = block_size - 1;
-            map[start_block] = new int[block_size]();
-        }
-    }
+		if (start_offset < 0) {
+			start_block--;
+			start_offset = block_size - 1;
+			map[start_block] = new int[block_size]();
+		}
+	}
 
-    void push_back(int x) {
-        if (finish_block > map_size) {
-            resize();
-        }
+	void push_back(int x) {
+		if (finish_block > map_size) {
+			resize();
+		}
 
-        map[finish_block][finish_offset] = x;
-        finish_offset++;
+		map[finish_block][finish_offset] = x;
+		finish_offset++;
 
-        if (finish_offset >= block_size) {
-            finish_block++;
-            finish_offset = 0;
-            map[finish_block] = new int[block_size]();
-        } 
-    }
+		if (finish_offset >= block_size) {
+			finish_block++;
+			finish_offset = 0;
+			map[finish_block] = new int[block_size]();
+		}
+	}
 
-    void pop_front() {
-        start_offset++;
+	void pop_front() {
+		start_offset++;
 
-        if (start_offset >= block_size) {
-            start_offset = 0;
-            delete[] map[start_block];
-            start_block++;
-        }
-    }
+		if (start_offset >= block_size) {
+			start_offset = 0;
+			delete[] map[start_block];
+			start_block++;
+		}
+	}
 
-    void pop_back() {
-        finish_offset--;
+	void pop_back() {
+		finish_offset--;
 
-        if (finish_offset < 0) {
-            finish_offset = block_size - 1;
-            delete[] map[finish_block];
-            finish_block--;
-        }
-    }
+		if (finish_offset < 0) {
+			finish_offset = block_size - 1;
+			delete[] map[finish_block];
+			finish_block--;
+		}
+	}
 };
 
 int main()
 {
-    Deque deque_1;
+	Deque deque_1;
 
-    deque_1.push_front(2);
-    deque_1.push_front(4);
-    deque_1.push_front(6);
-    /*
-    deque_1.push_front(1);
-    deque_1.push_front(2);
-    deque_1.push_front(3);
-    deque_1.push_front(4);
-    deque_1.push_front(5);
-    */
-    deque_1.push_back(8);
-    deque_1.push_back(1);
-    deque_1.push_back(3);
-    deque_1.push_back(5);
-    deque_1.push_back(5);
-    deque_1.push_back(4);
-    deque_1.push_back(12);
-    /*
-    deque_1.push_back(1);
-    deque_1.push_back(2);
-    deque_1.push_back(3);
-    deque_1.push_back(4);
-    deque_1.push_back(5);
-    */
-    deque_1.push_back(1);
-    deque_1.push_back(2);
-    deque_1.push_back(3);
-    deque_1.push_back(4);
-    deque_1.push_back(5);
-    deque_1.push_back(1);
-    deque_1.push_back(2);
-    deque_1.push_back(3);
-    deque_1.push_back(4);
-    deque_1.push_back(5);
-    deque_1.push_back(5);
-    deque_1.print();
+	deque_1.push_front(2);
+	deque_1.push_front(4);
+	deque_1.push_front(6);
+	/*
+	deque_1.push_front(1);
+	deque_1.push_front(2);
+	deque_1.push_front(3);
+	deque_1.push_front(4);
+	deque_1.push_front(5);
+	*/
+	deque_1.push_back(8);
+	deque_1.push_back(1);
+	deque_1.push_back(3);
+	deque_1.push_back(5);
+	deque_1.push_back(5);
+	deque_1.push_back(4);
+	deque_1.push_back(12);
+	/*
+	deque_1.push_back(1);
+	deque_1.push_back(2);
+	deque_1.push_back(3);
+	deque_1.push_back(4);
+	deque_1.push_back(5);
+	*/
+	deque_1.push_back(1);
+	deque_1.push_back(2);
+	deque_1.push_back(3);
+	deque_1.push_back(4);
+	deque_1.push_back(5);
+	deque_1.push_back(1);
+	deque_1.push_back(2);
+	deque_1.push_back(3);
+	deque_1.push_back(4);
+	deque_1.push_back(5);
+	deque_1.push_back(5);
+	deque_1.print();
 
-    deque_1.pop_back(); deque_1.print();
-    deque_1.pop_back(); deque_1.print();
-    deque_1.pop_back(); deque_1.print();
-    deque_1.pop_back(); deque_1.print();    
-    deque_1.pop_back();  deque_1.print();   
-    deque_1.pop_front(); deque_1.print();     
-    deque_1.pop_front(); deque_1.print();    
-    deque_1.pop_front(); deque_1.print();    
-    deque_1.pop_front(); deque_1.print();    
-    deque_1.pop_front(); deque_1.print();
-    std::cout<<"end"<<std::endl;
+	deque_1.pop_back(); deque_1.print();
+	deque_1.pop_back(); deque_1.print();
+	deque_1.pop_back(); deque_1.print();
+	deque_1.pop_back(); deque_1.print();
+	deque_1.pop_back();  deque_1.print();
+	deque_1.pop_front(); deque_1.print();
+	deque_1.pop_front(); deque_1.print();
+	deque_1.pop_front(); deque_1.print();
+	deque_1.pop_front(); deque_1.print();
+	deque_1.pop_front(); deque_1.print();
+	std::cout << "end" << std::endl;
 
 }
 
