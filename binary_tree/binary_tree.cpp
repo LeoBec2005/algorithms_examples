efe52796b4eb234763b18a7acf494123f9537b0c
Leonardo Becerra <leonardobecerra38@gmail.com>
2025-10-01 18:01:56 -0500

    feat: add binary tree with insert and in_order print

--- binary_tree/binary_tree.cpp
+++ binary_tree/binary_tree.cpp
@@ -1,7 +1,75 @@
 #include <iostream>
 
+struct Node {
+    int data;
+    Node* right = nullptr;
+    Node* left = nullptr;
+};
+
+class BinaryTree {
+private:
+    Node* head = nullptr;
+
+    void in_order(Node* next) {
+        if (next == nullptr)return;
+        in_order(next->left);
+        std::cout << next->data << " , ";
+        in_order(next->right);
+    }
+
+public:
+    void print_in_order() {
+        in_order(head);
+        std::cout<<std::endl;
+    }
+
+    void insert(int x) {
+        Node* new_node = new Node;
+        new_node->data = x;
+        if (head == nullptr) {
+            head = new_node;
+        }
+        else {
+            Node* temp = head;
+            while (true) {
+                if (temp->data > x) {
+                    if (temp->left == nullptr) break;
+                    temp = temp->left;
+                }
+                else if (temp->data < x) {
+                    if (temp->right == nullptr) break;
+                    temp = temp->right;
+                }
+            }
+            if (temp->data > x) {
+                temp->left = new_node;
+            }
+            else if (temp->data < x) {
+                temp->right = new_node;
+            }
+        }
+
+    }
+
+    bool find(int x) {
+
+    }
+
+
+};
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    BinaryTree tree_1;
+    tree_1.insert(10);
+    tree_1.insert(3);
+    tree_1.insert(2);
+    tree_1.insert(5);
+    tree_1.insert(6);
+    tree_1.insert(1);
+    tree_1.insert(0);
+
+    tree_1.print_in_order();
 }
 
