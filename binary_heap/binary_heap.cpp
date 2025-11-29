d64f74e3fe1d525517720888820daea75c42a2d6
LeoBec2005 <leonardobecerra38@gmail.com>
2025-11-29 11:22:34 -0500

    feat: add get_right_index, get_left_index and get_parent_index methods to BinaryHeap

--- binary_heap/binary_heap.cpp
+++ binary_heap/binary_heap.cpp
@@ -3,9 +3,23 @@
 
 class BinaryHeap {
 private:
+std::vector<int> heap_array;
+int get_right_index(int index){
+    return ((2*index)+2);
+}
+
+int get_left_index(int index){
+    return ((2*index)+1);
+}
+
+int get_parent_index(int index){
+    return ((index-1)/2);
+}
 
 public:
-
+void insert(int data){
+    heap_array.push_back(data);
+}
 
 };
 
