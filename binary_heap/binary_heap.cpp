e1cfb33a2a17c4f4d7df2262597614d2c17cee89
Leonardo Becerra <leonardobecerra38@gmail.com>
2025-11-30 22:56:40 -0500

    feat: add insert and print methods and refactor get_index method in BinaryHeap

--- binary_heap/binary_heap.cpp
+++ binary_heap/binary_heap.cpp
@@ -16,9 +16,27 @@ int get_parent_index(int index){
     return ((index-1)/2);
 }
 
+void vector_swap(int index1,int index2) {
+    int data = heap_array.at(index1);
+    heap_array.at(index1) = heap_array.at(index2);
+    heap_array.at(index2) = data;
+}
+
 public:
 void insert(int data){
     heap_array.push_back(data);
+    int data_index = heap_array.size()-1;
+    while ((data_index > 0) and (heap_array.at(get_parent_index(data_index)) < heap_array.at(data_index))) {
+            vector_swap(get_parent_index(data_index), data_index);
+            data_index = get_parent_index(data_index);
+    }
+}
+
+void print() {
+    for (auto it = heap_array.begin(); it != heap_array.end(); it++) {
+        std::cout << *it << " , ";
+    }
+    std::cout<<std::endl;
 }
 
 };
@@ -26,6 +44,15 @@ void insert(int data){
 
 int main()
 {
-    
+    BinaryHeap heap;
+    heap.insert(3);
+    heap.insert(5);
+    heap.insert(1);
+    heap.insert(0);
+    heap.insert(10);
+    heap.insert(2);
+
+    heap.print();
+
 }
 
